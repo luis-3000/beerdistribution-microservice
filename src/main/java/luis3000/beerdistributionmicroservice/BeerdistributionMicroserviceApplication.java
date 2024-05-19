415a441a0d5bc888da7100ecbce90a5d48aa0b0c
Jose Luis Castillo <josecastillocs@gmail.com>
2024-05-18 20:51:57 -0500

    Initial project added files

---
+++ src/main/java/luis3000/beerdistributionmicroservice/BeerdistributionMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package luis3000.beerdistributionmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerdistributionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerdistributionMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/luis3000/beerdistributionmicroservice/BeerdistributionMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package luis3000.beerdistributionmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerdistributionMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
